4c4d59079837d7458cf88ee567867463775d0460
Edgar Ravenhorst <edgar@code.rehab>
2017-04-16 18:40:43 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,4 @@
+require('./sass/site.scss');
+
+var Menu = require('advanced-menus');
+var menu = new Menu();
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+var path = require('path');
+var BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: './index.js',
+  watch: true,
+  output: {
+    filename: 'site.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'stage-0']
+        }
+      },
+      { // regular css files
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({
+          loader: 'css-loader?importLoaders=1',
+        }),
+      },
+      { // sass / scss loader for webpack
+        test: /\.(sass|scss)$/,
+        loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
+      }
+    ]
+  },
+  plugins: [
+    new BrowserSyncPlugin ({
+      // browse to http://localhost:3000/ during development,
+      // ./public directory is being served
+      open: 'external',
+      host: 'localhost',
+      proxy: 'menustest.mod',
+      files: ['resources/js/*.js', 'resources/images/**']
+    }),
+    new ExtractTextPlugin({ filename: 'site.css', disable: false, allChunks: true }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: { warnings: false }
+    })
+  ]
+};
