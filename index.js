7d965efbacea8b3069dc13a7eda3489a6b00c996
Edgar Ravenhorst <edgar@code.rehab>
2017-04-17 02:12:03 +0200

    multiple menu support(css only)

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ require('./sass/site.scss');
 
 var Menu = require('advanced-menus');
 var menu = new Menu();
+// var innermenu = new Menu({
+//   selectors:{
+//     menu_id: 'page-menu'
+//   }
+// });
