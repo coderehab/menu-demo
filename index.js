282949ea76ea3a7b0c3026fe3b27645208d67639
Edgar Ravenhorst <edgar@code.rehab>
2017-04-17 15:53:37 +0200

    complete multi menu support

--- index.js
+++ index.js
@@ -1,9 +1,45 @@
 require('./sass/site.scss');
 
 var Menu = require('advanced-menus');
-var menu = new Menu();
-// var innermenu = new Menu({
-//   selectors:{
-//     menu_id: 'page-menu'
-//   }
-// });
+
+var menu = new Menu({
+  selectors:{
+    toggle_button: ".menu-toggle-button",
+  },
+  options:{
+    disable_scroll: true,
+    use_handles:true,
+  },
+  events:{
+    swipeLeft: function(){
+      this.close();
+    },
+    swipeRight: function(){
+      this.open();
+    }
+  }
+});
+
+var innermenu = new Menu({
+  options:{
+    use_handles:true,
+  },
+  selectors:{
+    menu_id: 'page-menu'
+  }
+});
+
+var allmenusbyclass = document.querySelectorAll(".some-menu")
+for(var i = 0; i< allmenusbyclass.length; i++){
+  var id = allmenusbyclass[i].id;
+  if (!id) {
+    id = "am-menu-" + i;
+    allmenusbyclass[i].id = id;
+  }
+
+  var somemenu = new Menu({
+    selectors: {
+      menu_id: id
+    }
+  })
+}
